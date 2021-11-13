04d7d44e998f588e11d3a2603a1811b983b6305b
csontosreka <61116319+csontosreka@users.noreply.github.com>
2021-11-13 21:10:43 +0100

    updated StartPageController

--- SudokuApp/src/main/java/controller/StartPageController.java
+++ SudokuApp/src/main/java/controller/StartPageController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import generators.LevelGenerator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import org.tinylog.Logger;
@@ -11,15 +12,27 @@ public class StartPageController {
     @FXML
     private void handleEasyButton(ActionEvent event) throws IOException {
         Logger.debug("Starting easy level.");
+
+        LevelGenerator sudoku = new LevelGenerator(9, 20);
+        sudoku.fillValues();
+        sudoku.printSudoku();
     }
 
     @FXML
     private void handleMediumButton(ActionEvent event) throws IOException{
         Logger.debug("Starting medium level.");
+
+        LevelGenerator sudoku = new LevelGenerator(9, 40);
+        sudoku.fillValues();
+        sudoku.printSudoku();
     }
 
     @FXML
     private void handleHardButton(ActionEvent event) throws IOException{
         Logger.debug("Starting hard level.");
+
+        LevelGenerator sudoku = new LevelGenerator(9, 60);
+        sudoku.fillValues();
+        sudoku.printSudoku();
     }
 }
